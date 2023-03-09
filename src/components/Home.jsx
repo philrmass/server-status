1e0a9d98e7126b9f3103730f74b9e2becc6c87cc
Phil Mass <philrmass@gmail.com>
2023-03-09 00:14:21 -0800

    comments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,9 @@ import { version } from '../../package.json';
 import Sonos from './Sonos';
 import Status from './Status';
 
+// ??? add a logo
+// ??? deploy to server
+// ??? make music server
 export default function Home() {
   const defaultAddress = '192.168.1.2';
   const [address, setAddress] = useLocalStorage('stAddress', defaultAddress);
